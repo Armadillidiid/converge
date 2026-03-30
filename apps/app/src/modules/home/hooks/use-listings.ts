278b865bda78116569a58d001827550f38ab95b7
Emmanuel Isenah <emmanuelisenah@gmail.com>
2026-03-30 15:57:33 +0100

    refactor: remove legacy home module now replaced by chat redirect

--- apps/app/src/modules/home/hooks/use-listings.ts
+++
@@ -1,7 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import { type ListingsListData } from "@repo/sdk";
-import { listingsListOptions } from "@repo/sdk/tanstack";
-
-export function useListings(query: ListingsListData["query"]) {
-  return useQuery(listingsListOptions({ query }));
-}
